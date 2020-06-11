895a70100afc4621d67d26d606af6eadaaddae6e
Jessica Petrochuk <jesspe@gmail.com>
2020-06-10 20:49:21 -0700

    June 10th 2020

--- app.mjs
+++ app.mjs
@@ -30,10 +30,48 @@ var jobSchema = new mongoose.Schema({
 })
 var Job = mongoose.model("Job", jobSchema)
 
+// Home page
+app.get("/", function(req, res){
+  res.render("index")
+});
+
+// Index route
+app.get("/jobs", function(req, res){
+    Job.find({}, function(err, jobs){
+    if(err){
+      console.log(err);
+    } else{
+      res.render("jobs", {jobs: jobs});
+    }
+  })
+})
+
 // New route
 app.get("/jobs/new", function(req, res){
-  res.render("new.html")
+  res.render("new")
 });
+
+// Create route
+app.post("/jobs", function(req, res){
+  Job.create(req.body.job, function(err, newJob){
+      if(err){
+          res.render("new");
+      } else{
+          res.redirect("/jobs");
+      }
+  });
+})
+
+// Show route
+app.get("jobs/:id", function(req, res){
+  Job.findById(req.params.id , function(err, foundJob){
+    if(err){
+      res.redirect("/jobs");
+    } else{
+      res.render("show", {job: foundJob})
+    }
+  })
+})
 
 app.listen(3000, () =>
   console.log('App listening on port 3000!'),
